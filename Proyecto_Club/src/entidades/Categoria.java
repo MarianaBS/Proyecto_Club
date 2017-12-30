7018499eb6d5e0a5a5e88329161f94b694d4235d
MarianaBS <marianabsanchez@hotmail.com>
2017-12-30 20:12:51 -0300

    carga inicial

--- Proyecto_Club/src/entidades/Categoria.java
+++ Proyecto_Club/src/entidades/Categoria.java
@@ -1,5 +1,41 @@
 package entidades;
 
-public class Categoria {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Categoria {
+	private int id_categoria;
+	private String descripcion_cat;
+	private Date fecha_baja;
+	private ArrayList<Dto_Categ> descuentos;
+	
+	public ArrayList<Dto_Categ> getDescuentos() {
+		return descuentos;
+	}
+	public void setDescuentos(ArrayList<Dto_Categ> descuentos) {
+		this.descuentos = descuentos;
+	}
+	public int getId_categoria() {
+		return id_categoria;
+	}
+	public void setId_categoria(int id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+	public String getDescripcion_cat() {
+		return descripcion_cat;
+	}
+	public void setDescripcion_cat(String descripcion_cat) {
+		this.descripcion_cat = descripcion_cat;
+	}
+	public Date getFecha_baja() {
+		return fecha_baja;
+	}
+	public void setFecha_baja(Date fecha_baja) {
+		this.fecha_baja = fecha_baja;
+	}
+
+	public Categoria(String desc, ArrayList<Dto_Categ>dtos){
+		this.setDescripcion_cat(desc);
+		this.setDescuentos(dtos);
+	}
 }
--- Proyecto_Club/src/entidades/Socio.java
+++ Proyecto_Club/src/entidades/Socio.java
@@ -1,5 +1,86 @@
 package entidades;
 
 public class Socio {
+	
+	private int id_socio;
+	private String dni;
+	private String nombre_socio;
+	private String apellido_socio;
+	private String direccion;
+	private String telefono;
+	private String mail_socio;
+	private String usuario_soc;
+	private String password_soc;
+	private String estado;
+	private Categoria cat_soc;
+	
+	public int getId_socio() {
+		return id_socio;
+	}
+	public void setId_socio(int id_socio) {
+		this.id_socio = id_socio;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre_socio() {
+		return nombre_socio;
+	}
+	public void setNombre_socio(String nombre_socio) {
+		this.nombre_socio = nombre_socio;
+	}
+	public String getApellido_socio() {
+		return apellido_socio;
+	}
+	public void setApellido_socio(String apellido_socio) {
+		this.apellido_socio = apellido_socio;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getMail_socio() {
+		return mail_socio;
+	}
+	public void setMail_socio(String mail_socio) {
+		this.mail_socio = mail_socio;
+	}
+	public String getUsuario_soc() {
+		return usuario_soc;
+	}
+	public void setUsuario_soc(String usuario_soc) {
+		this.usuario_soc = usuario_soc;
+	}
+	public String getPassword_soc() {
+		return password_soc;
+	}
+	public void setPassword_soc(String password_soc) {
+		this.password_soc = password_soc;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public Categoria getCat_soc() {
+		return cat_soc;
+	}
+	public void setCat_soc(Categoria cat_soc) {
+		this.cat_soc = cat_soc;
+	}
+	
+	
 
 }
